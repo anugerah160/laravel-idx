1d45815ad16e38d9733332b9422e3219b51c1bfe
artuphareguna10 <artuphareguna10@gmail.com>
2024-08-23 15:51:15 +0000

    cek

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About page</title>
+</head>
+<body>
+    <h1>halaman About</h1>
+    <ul>
+        <li><a href="contact">contact</a></li>
+        <li><a href="about">about</a></li>
+        <li><a href="home">home</a></li>
+    </ul>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+</head>
+<body>
+    <h1>halaman Home</h1>
+    <ul>
+        <li><a href="contact">contact</a></li>
+        <li><a href="about">about</a></li>
+        <li><a href="home">home</a></li>
+    </ul>
+</body>
+</html>
